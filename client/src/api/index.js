9dfccffe13ab761629f1eb862ef0c2d34ecad83b
Ali Kalout <63719442+alikk351@users.noreply.github.com>
2021-07-23 22:23:08 +0300

    change api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://randoquizback.herokuapp.com" });
 
 export const fetchQuiz = form => API.post("/", form);
 export const sumbitQuiz = (answers, uuid) => API.post("/submit", { answers: answers, uuid: uuid });
